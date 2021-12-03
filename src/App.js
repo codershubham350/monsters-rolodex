9669737b6fce5b2323277a75b19ab1716283d636
Shubham Saxena <shubhamsaxena350@gmail.com>
2021-12-03 19:35:09 +0530

    Mosters App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+    // bind() is a method on any function that returns a new function,
+    // where context of this is set to whatever we passed to it
+    //this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    // Arrow function automatically bing this at the first place in the context of which it is defined.
+    // Here this will point to the App component extending Component
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    // to use the state objects inside render()
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monster Rolodex </h1>
+        <label
+          style={{ fontWeight: "bold", marginRight: "10px", color: "#fff" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Search 🔎
+        </label>
+
+        <SearchBox
+          placeholder="Search your Monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
